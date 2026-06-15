e01d514a1788b2dab16924a8fe06db4aa59408ed
MohitJaiswal2507 <mohitjaiswal2507@gmail.com>
2026-06-15 22:52:15 +0530

    linked list started

---
+++ LinkedList/DeletionLL.java
@@ -0,0 +1,105 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data1){
+        this.data = data1;
+        this.next = null;
+    }
+
+    Node(int data1 , Node next1){
+        this.data = data1;
+        this.next = next1;
+    }
+}
+
+public class DeletionLL {
+
+    private static Node ArrtoLL(int[] arr){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+
+        for(int i=1 ; i<arr.length ; i++){
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    } 
+
+    public static void printLL(Node head){
+        
+        while(head != null){
+            System.out.print(head.data+" ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    // public static Node removeHead(Node head){
+    //     if(head == null) return head;
+    //     head = head.next;
+    //     return head;
+    // } 
+
+    // private static Node removeTail(Node head){
+    //     if(head == null || head.next == null) return null; //Edge case if linked list is [null] or [12]->null
+
+    //     Node temp = head;
+    //     //Going to the 2nd last element 
+    //     while(temp.next.next != null){
+    //         temp = temp.next;
+    //     } 
+    //     temp.next = null; // 2nd last ko nuullptr dedo
+    //     return head;
+    // }
+    
+    private static Node removeKthNode(Node head, int k){
+        if(head == null) return null;
+
+        if(k==1) {
+            // Node temp = head;
+            head = head.next;
+            return head;
+        }
+
+        int cnt=0 ; Node temp = head ; Node prev = null; 
+        while(temp != null){
+            cnt++;
+            if(cnt == k){
+                prev.next = prev.next.next;
+                break;
+        }
+            prev = temp;
+            temp = temp.next;
+        }
+
+        if(k > cnt) return null; // suppose 5 dediya tab null dedena
+        
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {12,5,6,8};
+        Node head =  ArrtoLL(arr);
+
+        System.out.print("LL before deletion ");
+        printLL(head);
+        System.out.print("Head before deletion " + head.data);
+
+        System.out.println();
+
+        // head = removeHead(head);
+        // printLL(head);
+        // System.out.print("Head after deletion " + head.data);
+
+        // head = removeTail(head);
+        // printLL(head);
+        // System.out.print("Head after Tail deletion " + head.data);
+
+        System.out.print("LL after deletion of kth element => ");
+        head = removeKthNode(head, 4);
+        printLL(head);
+
+    }
+}
---
+++ LinkedList/Linkedlist.java
@@ -0,0 +1,85 @@
+class Node{
+    int data;
+    Node next;
+
+    //Default contructor
+    Node(int data1){
+        this.data = data1;
+        this.next = null;
+    }
+
+    //parametrised constructo
+    Node(int data1, Node next1){
+        this.data = data1;
+        this.next = next1;
+    }
+
+}
+
+public class Linkedlist {
+
+    private static Node ArrtoLL(int[] arr){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+
+        for(int i=1 ; i<arr.length ; i++){
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    } 
+
+    private static int lengthofLL(Node head){
+        int cnt = 0;
+        Node temp = head;
+        System.out.println();
+
+        while(temp != null){
+            temp = temp.next;
+            cnt++;
+        }
+        return cnt;
+
+    }
+
+    private static boolean checkifPresent(Node head, int val){
+        Node temp = head;
+        while(temp != null){
+            if(temp.data == val) return true;
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10,12,17,23,45};
+
+        Node x = new Node(arr[3]);
+        System.out.println("Array 3 address at "+x);
+        System.out.println("Array 3 at "+x.data);
+        System.out.println("Array 3 next pointing to "+x.next);
+
+        //Array to Linked List conversion
+        Node head = ArrtoLL(arr);
+        System.out.println("Arr to LL conversion head "+head.data);
+
+        //Traversal
+        Node temp = head;
+        System.out.print("Linked list to arr => ");
+        while(temp != null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+
+        //Length of LL
+        System.out.print("Length of LL => "+lengthofLL(head));
+        System.out.println();
+
+        //Checking in LL
+        System.out.println("Is 23 present in LL => "+checkifPresent(head, 23));
+        System.out.println("Is 69 present in LL => "+checkifPresent(head, 69));
+
+
+    }
+}
