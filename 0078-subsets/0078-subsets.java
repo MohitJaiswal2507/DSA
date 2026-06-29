746aed44e1940fac5e4c371d043c011dcf7b6106
MOHIT JAISWAL <158329990+MohitJaiswal2507@users.noreply.github.com>
2026-06-29 21:41:37 +0530

    Time: 1 ms (88.2%), Space: 44.2 MB (38.71%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,28 +1,28 @@
 class Solution {
 
-    public void generate(int index, int[] nums, List<Integer> ds, List<List<Integer>> ans) {
+    public void findSubsets(int ind, int[] nums, List<Integer> ds, List<List<Integer>> ans) {
 
         // Base case
-        if (index == nums.length) {
-            ans.add(new ArrayList<>(ds)); // Store current subset
+        if (ind == nums.length) {
+            ans.add(new ArrayList<>(ds));
             return;
         }
 
-        // Take
-        ds.add(nums[index]);
-        generate(index + 1, nums, ds, ans);
+        // Pick
+        ds.add(nums[ind]);
+        findSubsets(ind + 1, nums, ds, ans);
 
-        // Not Take then backtrack
+        // Backtrack
         ds.remove(ds.size() - 1);
-        generate(index + 1, nums, ds, ans);
+
+        // Not Pick
+        findSubsets(ind + 1, nums, ds, ans);
     }
 
     public List<List<Integer>> subsets(int[] nums) {
 
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> ds = new ArrayList<>();
-
-        generate(0, nums, ds, ans);
+        findSubsets(0, nums, new ArrayList<>(), ans);
 
         return ans;
     }
