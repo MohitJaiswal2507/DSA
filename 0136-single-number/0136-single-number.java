0ca4be99b9c60fcbab0d1c5bfcd434377e3b55e9
MOHIT JAISWAL <158329990+MohitJaiswal2507@users.noreply.github.com>
2026-07-13 22:03:49 +0530

    Time: 1 ms (99.97%), Space: 46.8 MB (82.26%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,8 +1,5 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        //Edge case
-        if(nums.length == 0) return 0;
-
         int Xor = 0;
         for(int i=0; i<nums.length; i++){
             Xor = Xor ^ nums[i]; // 2^2 = 0 ^ 1 => 1
