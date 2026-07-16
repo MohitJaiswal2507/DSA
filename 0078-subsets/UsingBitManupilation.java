bba423b3a609e865a86f8770726ec8b4fa2ad23d
Mohit Jaiswal <mohitjaiswal2507@gmail.com>
2026-07-16 13:39:14 +0530

    Implement subset generation using bit manipulation

---
+++ 0078-subsets/UsingBitManupilation.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        int n = nums.length;
+        int totalSubsets = 1 << n;
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int mask = 0; mask < totalSubsets; mask++) {
+
+            List<Integer> subset = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+
+                if ((mask & (1 << i)) != 0) {
+                    subset.add(nums[i]);
+                }
+            }
+
+            ans.add(subset);
+        }
+
+        return ans;
+    }
+}
