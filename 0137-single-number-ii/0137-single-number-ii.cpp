6fc74a46e0b0b5bdf7eb8ca84b0b9b8dc1cf1f29
MOHIT JAISWAL <mohitjaiswal2507@gmail.com>
2026-07-27 11:34:45 +0530

    Time: 0 ms (100%), Space: 13.2 MB (87.31%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
-        for(int bitIndex = 0; bitIndex < 32; bitIndex++){
-            int cnt =0 ;
-            for(int i=0; i<nums.size(); i++){
-                if(nums[i] & (1 << bitIndex)) cnt++;
-            }
-            if(cnt % 3 ==1){
-                ans = ans | (1 << bitIndex);
-            }
+        // Bucket Concept
+        int ones = 0 , twos = 0;
+
+        for(int i=0; i<nums.size(); i++){
+            ones = (ones ^ nums[i]) & (~twos);
+            twos = (twos ^ nums[i]) & (~ones);
         }
-        return ans;
+        return ones;
     }
 };
